c4a143478d9ce986c19dcfb6af28484f20c41648
azo221017m-hub <azo221017m@gmail.com>
2025-09-04 12:12:08 -0600

    Subida forzada desde local

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,14 @@
 // backend/db.js
 require('dotenv').config();
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('✅ Conectado a MySQL');
-});
-
-module.exports = db;
+module.exports = pool;
