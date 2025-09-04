224b317372d119fa3579938c4b9ba96392e8ed0d
azo221017m-hub <azo221017m@gmail.com>
2025-09-04 14:02:08 -0600

    Subida forzada desde local

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ app.get('/api/visitas', (req, res) => {
   db.query(sql, (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     if (results.length > 0) {
-      res.json({ visitas: results[0].visitas });
+      res.json({ visitas: results[0].visitas }+1000);
     } else {
       res.json({ visitas: 0 });
     }
